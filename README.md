881c516a40270cb7f13ce6313c17ea8dab570a67
Axel Hodler <axel@hodler.co>
2017-08-26 22:50:51 +0200

    init sample test

---
+++ README.md
@@ -0,0 +1,3 @@
+# Initial Project for doing Katas in Kotlin
+
+Libraries are part of the repo to avoid spending any time with maven/gradle.
---
+++ src/test/kotlin/co/hodler/MainTest.kt
@@ -0,0 +1,13 @@
+package test.kotlin.co.hodler
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class MainTest {
+
+    @Test
+    fun `a test runs`() {
+        assertEquals(2, 1 + 1)
+    }
+
+}
